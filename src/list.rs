6f6b3cef75c782e7ca57191a1297503e85723c37
dave <wind2008062163.com>
2020-12-12 12:21:09 +0800

    add：leetcode答题

--- src/list.rs
+++ src/list.rs
@@ -1,14 +1,16 @@
-use std::collections::LinkedList;
+use std::collections::{HashMap, LinkedList};
 use std::iter::FromIterator;
 
 pub struct SimpleLinkedList<T> {
     head: Option<Box<Node<T>>>,
     len: usize,
 }
+
 struct Node<T> {
     data: T,
     next: Option<Box<Node<T>>>,
 }
+
 impl<T> Node<T> {
     pub fn new(data: T, next: Option<Box<Node<T>>>) -> Self {
         Node { data, next }
@@ -62,7 +64,7 @@ impl<T> SimpleLinkedList<T> {
 }
 
 impl<T> FromIterator<T> for SimpleLinkedList<T> {
-    fn from_iter<I: IntoIterator<Item = T>>(_iter: I) -> Self {
+    fn from_iter<I: IntoIterator<Item=T>>(_iter: I) -> Self {
         let mut list = Self::new();
         for x in _iter {
             list.push(x);
@@ -80,4 +82,53 @@ impl<T> Into<Vec<T>> for SimpleLinkedList<T> {
         }
         v
     }
+}
+
+pub fn length_of_longest_substring(s: String) -> i32 {
+    let s: Vec<char> = s.chars().collect();
+    let mut indices = HashMap::new();
+    let mut ret:usize = 0;
+    let mut i:usize = 0;
+
+    for j in 0..s.len() {
+        match indices.insert(s[j], j) {
+            Some(x) => i = i.max(x + 1),
+            None => (),
+        }
+        ret = ret.max(j - i + 1);
+    }
+
+    ret as i32
+}
+
+pub fn die_simulator(n: i32, roll_max: Vec<i32>) -> i32 {
+    const M: i32 = 1_000_000_007;
+    let mut dp = vec![vec![vec![0; 16]; 7]; n as usize];
+    (1..=6).for_each(|j| dp[0][j][0] = 1);
+    let mut ret = 0;
+
+    for i in 1..(n as usize) {
+        for j in 1..=6 {
+            let f = |x: usize| {
+                dp[i - 1][x][..(roll_max[x - 1] as usize)]
+                    .iter()
+                    .fold(0, |acc, x| (acc + x) % M)
+            };
+            dp[i][j][0] = (1..=6)
+                .filter(|&x| x != j)
+                .map(|x| f(x))
+                .fold(0, |acc, x| (acc + x) % M);
+            for k in 1..(roll_max[j - 1] as usize).min(i + 1) {
+                dp[i][j][k] = dp[i - 1][j][k - 1];
+            }
+        }
+    }
+
+    for j in 1..=6 {
+        for k in 0..(roll_max[j - 1].min(n) as usize) {
+            ret = (ret + dp[n as usize - 1][j][k]) % M;
+        }
+    }
+
+    ret
 }
